dd41c871dadf7256205adb9399b80fa0405a612d
Best think co <Best think co@192.168.1.109>
2020-02-29 21:49:53 +0200

    new

---
+++ src/ca/ca.java
@@ -0,0 +1,25 @@
+package ca;
+
+public class ca {
+	
+	
+
+	public static int add(int x,int y) {
+		if(x>0 & y>0)
+			return x+y;
+		else 
+			return -1;
+			
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+	}
+
+}
+
